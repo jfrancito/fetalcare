296267f340dbc4a469260ca81b5906e424babc32
Jorge Francelli <francito20al@gmail.com>
2022-11-23 11:01:02 -0500

    version

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
         
         Schema::defaultStringLength(191);    
         View::share('capeta', '/fetalcare');  
-        View::share('version', '1.1');
+        View::share('version', '1.2');
         View::share('titulo', '');
 
         Validator::extend('unico', function($attribute, $value, $parameters , $validator){
